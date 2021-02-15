80ec436f77cd436f22756a9cfb60944469982080
Royce551 <poohwie2005@gmail.com>
2021-02-15 09:01:55 -0600

    Add project files.

---
+++ Squidphotos/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Squidphotos.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Squidphotos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentFrame.Navigate(new ViewerPage(@"C:\Users\poohw\OneDrive\Desktop\stuff on desktop\Nekoparaiten-Ann_07-04-19_002(CUnet)(noise_scale)(Level0)(x2.000000).png"));
+        }
+    }
+}
---
+++ Squidphotos/Pages/ViewerPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Squidphotos.Pages
+{
+    /// <summary>
+    /// Interaction logic for ViewerPage.xaml
+    /// </summary>
+    public partial class ViewerPage : Page
+    {
+        private string filePath;
+        private MemoryStream image;
+        public ViewerPage(string filePath)
+        {
+            this.filePath = filePath;
+            InitializeComponent();
+            PresentImage(filePath);
+        }
+        public async void PresentImage(string filePath)
+        {
+            var image = new MemoryStream(await Task.Run(() => File.ReadAllBytes(filePath)));
+            BackgroundImage.Source = ForegroundImage.Source = BitmapFrame.Create(image, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            this.image = image;
+        }
+
+        private void Page_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var margin = ContentBorder.Margin;
+            var scrollValue = (e.Delta / 2.5) * -1;
+            margin.Bottom += scrollValue;
+            margin.Top += scrollValue;
+            margin.Left += scrollValue;
+            margin.Right += scrollValue;
+            ContentBorder.Margin = margin;
+        }
+    }
+}
