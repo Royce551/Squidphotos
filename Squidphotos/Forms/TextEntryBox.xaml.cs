1a59a5fdb274529b395f1dede6d48bba83f2e1c6
Royce551 <poohwie2005@gmail.com>
2021-02-15 13:03:40 -0600

    image switching

---
+++ Squidphotos/Forms/TextEntryBox.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Squidphotos.Forms
+{
+    /// <summary>
+    /// Interaction logic for TextEntryBox.xaml
+    /// </summary>
+    public partial class TextEntryBox : Window
+    {
+        public bool OK = false;
+        public string Response = "";
+        public TextEntryBox(string prompt, string boxstarttext = "")
+        {
+            InitializeComponent();
+            Label.Text = prompt;
+            Box.Text = boxstarttext;
+        }
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            OK = true;
+            Response = Box.Text;
+            Close();
+        }
+    }
+}
--- Squidphotos/Pages/ViewerPage.xaml.cs
+++ Squidphotos/Pages/ViewerPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using Squidphotos.Forms;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,26 +25,40 @@ namespace Squidphotos.Pages
         public readonly string[] AllowableFileTypes = {".bmp", ".jpg", ".jpeg", ".png", ".tiff", ".gif", ".ico"};
 
         private string filePath;
+        private List<string> imagesInDirectory = new List<string>();
+        private int currentImage = 0;
         private MemoryStream image;
         public ViewerPage(string filePath)
         {
             this.filePath = filePath;
             InitializeComponent();
-            PresentImage(filePath);
+            Init();
             ContentBorder.Interacted += ContentBorder_Interacted;
         }
-        public async void PresentImage(string filePath)
+        public async void Init()
         {
-            if (!AllowableFileTypes.Contains(Path.GetExtension(filePath)))
+            PositionTextBlock.Text = "Loading, please wait...";
+            await Task.Run(() => FindImagesInDirectory(filePath));
+            PresentImage();
+        }
+        public async void PresentImage()
+        {       
+            if (!AllowableFileTypes.Contains(Path.GetExtension(imagesInDirectory[currentImage])))
             {
                 MessageBox.Show("That file type can't be displayed :(");
                 return;
             }
-            var image = new MemoryStream(await Task.Run(() => File.ReadAllBytes(filePath)));
+            var image = new MemoryStream(await Task.Run(() => File.ReadAllBytes(imagesInDirectory[currentImage])));
+            PositionTextBlock.Text = $"{currentImage + 1}/{imagesInDirectory.Count}";
             BackgroundImage.Source = ForegroundImage.Source = BitmapFrame.Create(image, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
             this.image = image;
         }
-
+        public void FindImagesInDirectory(string filePath)
+        {
+            imagesInDirectory.Clear();
+            imagesInDirectory.AddRange(Directory.EnumerateFiles(Path.GetDirectoryName(filePath), "*.*", SearchOption.TopDirectoryOnly).Where(x => AllowableFileTypes.Contains(Path.GetExtension(x))));
+            currentImage = imagesInDirectory.FindIndex(y => y == filePath);
+        }
         private void Page_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             var margin = ContentBorder.Margin;
@@ -61,13 +76,50 @@ namespace Squidphotos.Pages
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
             var openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true) PresentImage(openFileDialog.FileName);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+                Init();
+            }
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             ContentBorder.Reset();
             ResetButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentImage > 0)
+            {
+                currentImage--;
+                PresentImage();
+            }
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentImage < imagesInDirectory.Count - 1)
+            {
+                currentImage++;
+                PresentImage();
+            }
+        }
+
+        private void PositionTextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var box = new TextEntryBox("What image do you want to jump to?", (currentImage + 1).ToString());
+            box.ShowDialog();
+            if (box.OK)
+            {
+                if (int.TryParse(box.Response, out int result))
+                {
+                    currentImage = result - 1;
+                    PresentImage();
+                }
+                else MessageBox.Show("That's not a valid image.");
+            }
         }
     }
 }
